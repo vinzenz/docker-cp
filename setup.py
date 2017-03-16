9ce07f3e06f8ee5c846069ed8da455851a95a574
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-03-16 12:33:31 +0100

    Added URL
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(name='docker_copy',
       description='Copies files/folders from and to containers',
       author='Vinzenz Feenstra',
       author_email='evilissimo@redhat.com',
-      url='',
+      url='https://github.com/vinzenz/docker-cp',
       packages=['docker_copy'],
+      install_requires=['docker>=2.1.0'],
       entry_points={
           'console_scripts': ['docker-cp=docker_copy:main']})
