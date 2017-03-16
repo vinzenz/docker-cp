365d85ee14c6d7f8009b1f7bbd6d34933dd93d8b
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-03-16 23:43:31 +0100

    Some new test cases for the new TarStream implementation
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- tests/test_tarstream.py
+++ tests/test_tarstream.py
@@ -17,14 +17,50 @@
 #
 # Refer to the README and COPYING files for full details of the license.
 #
+import inspect
+import os
 import unittest
+
+from docker_copy.tarstream import TarStream
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+TEST_DATA_DIR = os.path.join(SCRIPT_DIR, 'data')
 
 
 class TestTarStream(unittest.TestCase):
 
-    def test_success(self):
-        pass
+    def test_add_item_size_verifications(self):
+        ts = TarStream(8, SCRIPT_DIR)
+        ts.add_item('data')
+        # Header
+        #   512
+        self.assertEqual(ts.tell(), 512)
+        ts.add_item('data/file1.txt')
+        # Header + Header + (File + padding)
+        #   512  +  512   +      512
+        self.assertEqual(ts.tell(), 1536)
+        ts.finalize()
+        # Header + Header + (File + padding) + finalization
+        #   512  +  512   +      512         +     1024
+        self.assertEqual(ts.tell(), 2560)
+        ts.close()
 
+    def test_applied_buffer_size8(self):
+        ts = TarStream(8, SCRIPT_DIR)
+        ts.finalize()
+        # 1024 bytes => 128 * 8 bytes
+        count = len([x for x in ts if len(x) == 8])
+        self.assertEqual(count, 128)
+        ts.close()
+
+    def test_applied_buffer_size2(self):
+        ts = TarStream(2, SCRIPT_DIR)
+        ts.finalize()
+        # 1024 bytes => 512 * 2 bytes
+        count = len([x for x in ts if len(x) == 2])
+        self.assertEqual(count, 512)
+        ts.close()
 
 if __name__ == '__main__':
     unittest.main()
