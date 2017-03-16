4b113f9f47867d44dfccbd979a756a9e5367e4dd
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-03-16 12:49:34 +0100

    A bit more documentation
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- docker_copy/error.py
+++ docker_copy/error.py
@@ -2,6 +2,7 @@
 
 
 class UsageError(Exception):
+    """ Indicator for docker_copy usage errors """
 
     def __init__(self, message, *args, **kwargs):
         super(UsageError, self).__init__(message, *args, **kwargs)
@@ -9,18 +10,21 @@ class UsageError(Exception):
 
 
 class InvalidContainerSpec(Exception):
+    """ Used when the specification for a container had an invalid form  """
 
     def __init__(self, *args, **kwargs):
         super(UsageError, self).__init__(*args, **kwargs)
 
 
 class ContainerNotFound(UsageError):
+    """ Used when a container couldn't be found  """
 
     def __init__(self, *args, **kwargs):
         super(UsageError, self).__init__(*args, **kwargs)
 
 
 class ItemNotFound(UsageError):
+    """ Used when the filesystem item couldn't be found in the container  """
 
     def __init__(self, *args, **kwargs):
         super(UsageError, self).__init__(*args, **kwargs)
--- docker_copy/tarstream.py
+++ docker_copy/tarstream.py
@@ -6,6 +6,13 @@ from io import BytesIO
 
 
 class TarStream(object):
+    """ Implements a file like object for uploading a file in tar archive
+    format to a docker container via the docker API. The whole thing by using
+    a fixed size buffer for the file upload.
+
+    This class is specialized on the usecase and the file API only returns
+    what is necessary to fulfil requests needs on the API.
+    """
 
     def __init__(self, header, stream, ssize, padding, block_size):
         self._chain = [BytesIO(header), stream, BytesIO(padding)]
@@ -16,21 +23,37 @@ class TarStream(object):
                 'TarStream not aligned: size = {0}'.format(self._size))
 
     def tell(self):
+        """ Returns always just the size"""
         return self._size
 
     def seek(self, off, direction):
+        """ Won't do anything since requests would just jump to the end and
+        the start to get the size of the file like object
+        """
         return
 
     def __iter__(self):
+        """ Comply with the iterator interface """
         return self
 
     def __next__(self):
+        """ Comply with the iterator interface (python 3) """
         return self.next()
 
     def next(self):
+        """ Comply with the iterator interface return max block_size bytes """
+
+        # We're done when the chain is empty
         if not self._chain:
             raise StopIteration()
+
         buf = bytes()
+
+        # Explanation of the following loop:
+        # While there is not enough and the chain is not empty read 0 to
+        # block_size bytes from the first element in the chain.
+        # When the the element hit the end, pop it from the list and continue
+        # unless the list is empty which will break the loop and we'll exit
         while len(buf) < self._block_size and self._chain:
             rsize = self._block_size - len(buf)
             rbuf = self._chain[0].read(rsize)
